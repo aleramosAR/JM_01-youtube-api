4a0a65f21c60a410c11c22058aeb81dc5c3bc8c2
aleramosAR <aleramos.ok@gmail.com>
2022-10-09 23:36:42 -0300

    React - Youtube API

---
+++ src/components/VideoItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Grid, Paper, Typography } from '@mui/material';
+
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <Grid item xs={12}>
+      <Paper style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} onClick={() => onVideoSelect(video)}>
+        <img style={{ marginRight: '20px', width: '50%' }} alt='thumbnail' src={video.snippet.thumbnails.medium.url} />
+        <Typography variant="subtitle2"><b>{video.snippet.title}</b></Typography>
+      </Paper>
+    </Grid>
+  )
+}
+
+export default VideoItem;
